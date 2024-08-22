daf318ab7223423daa5e1ba3da258e4752d715b3
kavirasanE <einsteinkavi98@gmail.com>
2024-08-22 08:47:04 +0530

    Changes

--- backend/controller/TestDoneTableController.js
+++ backend/controller/TestDoneTableController.js
@@ -11,26 +11,25 @@ const createTestDoneTable = (req, res) => {
   const DateNo = parseInt(colNo[2]);
   console.log(Month);
   const values = [req.body.Analyte_Name, req.body.TestDoneCount, Month, year];
-
   const existQuery =
     "SELECT EXISTS(SELECT 1 FROM test_done WHERE Month = ?) AS month_exists;";
   const existValue = [Month];
-  dbConfig.query(existQuery, [existValue], (err, data) => {
+  dbConfig.query(existQuery, existValue, (err, data) => {
     if (err) {
       return res.status(400).send(err.message);
     } else {
       const MonthBoolean = data[0].month_exists;
       console.log(MonthBoolean, "exist or not ");
       if (MonthBoolean > 0) {
-        UpdateTestFormTable(values, DateNo);
+        UpdateTestFormTable([...values,req.body.Analyte_Name], DateNo);
       } else {
         CreateNewTestForm(values, DateNo);
       }
     }
-  });
+  }); 
 
   const CreateNewTestForm = (datas, col) => {
-    const query = `INSERT INTO Test_Done (Analyte_Name, col${col} ,Month, Year) VALUES (?);`;
+    const query = `INSERT INTO Test_Done (Analyte_Name, col${col} ,Month, Year) VALUES (?,?,?,?);`;
     dbConfig.query(query, [datas], (err, data) => {
       if (err) {
         console.log(err.message);
@@ -64,8 +63,10 @@ const createTestDoneTable = (req, res) => {
     const updateQuery = `UPDATE test_done SET Analyte_Name = ?, col${col} = ?, Month = ?, Year = ? WHERE Analyte_Name = ?`;
     console.log(updateQuery,"update the query")
     console.log(datas); // Log the data being passed for debugging
+
     
-    dbConfig.query(updateQuery, datas, (err, data) => {
+    
+    dbConfig.query(updateQuery, [...datas], (err, data) => {
         if (err) {
             console.log(err.message);
             return res.status(400).send(err.message);
